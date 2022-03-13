e89999b0c8839fdea9caaec57f4d208ed77435d5
Marta <martamaleno.185@hotmail.com>
2022-03-13 12:38:50 +0100

    Enunciado

---
+++ practica1.sql
@@ -0,0 +1,37 @@
+-- Prueba: Diccionario de Oracle
+
+-- PREGUNTA 1
+-- Muestre el nombre y el tipo de los atributos de la tabla ALUMNOS del usuario DOCENCIA
+
+
+-- PREGUNTA 2
+-- Listar el nombre y tipo de todos los objetos del usuario con el que te has conectado
+
+
+-- PREGUNTA 3
+-- Mostrar los roles que el usuario tienen actualmente activados
+
+
+--PREGUNTA 4
+-- Listar los privilegios de sistema asignados al usuario conectado
+
+-- PREGUNTA 5
+-- Listar los nombres de restricciones foráneas a tablas del mismo esquema que tiene el usuario DOCENCIA
+
+-- PREGUNTA 6
+-- Lista, ordenado por el nombre de tabla todas las columnas, su tipo y su longitud del esquema DOCENCIA
+
+-- PREGUNTA 7
+-- Listar el nombre de las vistas que a su vez dependen de otras vistas. Mostrar también el nombre de esas otras vistas de las que dependen.
+
+-- PREGUNTA 8
+-- Listar el nombre de todas las vistas de usuarios que comiencen por UBD y que son INVALIDAS
+
+-- PREGUNTA 9
+-- Listar las Integridad referencial de las tablas del esquema DOCENCIA. Es decir, listar el nombre de la tabla, columna y nombre de la restricción foránea.
+
+-- PREGUNTA 10
+-- Seleccionar todas las vistas del usuario DOCENCIA basadas en una consulta larga (más de 150 caracteres).
+
+-- PREGUNTA 11
+-- Mostrar todos los índices marcados como únicos (UNIQUENESS='UNIQUE') del usuario DOCENCIA junto al número de columnas que lo forman.
